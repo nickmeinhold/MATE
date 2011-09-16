b1f6d36f3cc6ac5f464aa8ee4581418e880facb1
Nicholas Meinhold <nick@Nicholas-Meinholds-iMac.local>
2011-09-16 12:05:06 +1000

    Initial Commit

---
+++ MATE2/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  MATE2
+//
+//  Created by Nicholas Meinhold on 16/09/11.
+//  Copyright 2011 Cooperative Communities. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument {
+}
+
+@end
---
+++ MATE2/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MATE2
+//
+//  Created by Nicholas Meinhold on 16/09/11.
+//  Copyright 2011 Cooperative Communities. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
